191ef8f9c34d71cd5b85228baaeeaf6385caf81d
Rakshak Shetty <rakshak_81@hotmail.com>
2022-03-20 21:40:36 +0000

    step 2

--- models/user.js
+++ models/user.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 
-
-const userSchema = new mongoose.Schema({
-    name: {type:String, required: true},
-    userName: {type:String, required: true},
-    email: {type:String, required: true},
-    password: {type:String, required: true},
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String
+  
 });
 
 const User = mongoose.model("User", UserSchema);
